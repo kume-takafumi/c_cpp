50600a43ca7e980c40d6552cd671e22beee55a63
kume takafumi <46054448+kume-takafumi@users.noreply.github.com>
2023-04-19 04:48:24 +0900

    Add files via upload

---
+++ chat_array_split_parameter/stringsplit.cpp
@@ -0,0 +1,169 @@
+﻿//make dt
+//./stringsplit.cpp
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "./stringsplit.h"
+
+//----------------------------------------------------------------------s.
+int funcStringDataExchange(char *InputsCharX1d) {
+	//char str[256] = "abc,defgh,ij";
+	char str[1024] = "\0";
+	sprintf(str,"%s",InputsCharX1d);
+	const char *const separator = ",";
+	char *token = strtok( str, separator );
+	int size = 0;
+	while( token != NULL ){
+	    //puts( token );
+	    token = strtok( NULL, separator );
+		size++;
+	}
+	return size;
+}
+//----------------------------------------------------------------------e.
+
+//----------------------------------------------------------------------s.
+/*処理1: 用途:変換関数。1次元char型配列を->2次元char型が配列へ分割する処理。*/
+char **funcStringDataExchange(char **OutDataXY2d, char *InputsCharX1d, int ponoff)
+{
+	const char *const separator = ",";
+	char str[1024] = "\0";
+	sprintf(str,"%s",InputsCharX1d);	
+	char *token = strtok( str, separator );
+	int count = 0;
+	while( token != NULL ){
+		if(ponoff==2)printf("%s\n", token);
+		/*in*/strcpy( OutDataXY2d[count], token );
+	    token = strtok( NULL, separator );
+		count++;
+	}//ponoff = 1;
+	if(ponoff==1) {
+		for(int s = 0; s < count; s++) {
+			printf("%s ", OutDataXY2d[s]);
+		}printf("\n");
+	}
+return OutDataXY2d;
+}
+//----------------------------------------------------------------------e.
+
+//---------------------------------------------------------------------s.
+void mys_base_stringsplit::splitDataRegisterPushued(int ponoff)
+{
+	unutYLine = mymemory(unutYLine, unitXLine);
+	for(int x = 0; x < unitXLine; x++) {
+		unutYLine[x] = funcStringDataExchange(UserAnyArray1d[x]);
+	}	
+	split2darry = mymemory3d(split2darry, unitXLine, unutYLine);
+	for(int x = 0; x < unitXLine; x++) {
+		split2darry[x] = funcStringDataExchange( split2darry[x], UserAnyArray1d[x], (int)-1 );
+	}
+	if(ponoff==1) {
+	 	printf("//const format-------------------------------------s.\n");
+	 	for(int x = 0; x < unitXLine; x++) {
+	 		printf("%d(x): %2d(ys): %s\n", x, unutYLine[x], UserAnyArray1d[x]);
+	 	}
+	 	printf("split process -> -> <1d char> -> -> <2d char> -> ->\n");
+	 	for(int x = 0; x < unitXLine; x++) {
+	 		printf("%d(x): %2d(ys): ",x, unutYLine[x]);
+	 		for(int y = 0; y < unutYLine[x]; y++) {
+	 			printf("%s ", split2darry[x][y]);
+	 		} printf("\n");
+	 	}printf("//const format-------------------------------------e.\n");
+	}
+}//---------------------------------------------------------------------e.
+
+//---------------------------------------------------------------------s.
+void mys_base_stringsplit::splitDataRegisterPushued(mys_base_stringsplit::nestchar *request, int ponoff)
+{
+	if(ponoff==1)printf("//user any extra----------------------------------e.\n");
+	/*in*/UserAnyArray1d = mymemory(UserAnyArray1d, unitXLine);
+	for(int x = 0; x < unitXLine; x++) {
+		/*in*/strcpy( UserAnyArray1d[x], request[x].charArray );
+		if(ponoff==1)printf("%d, %s\n", funcStringDataExchange( request[x].charArray ), UserAnyArray1d[x] );
+	}
+	splitDataRegisterPushued((int)-1);
+}//---------------------------------------------------------------------e.
+
+//---------------------------------------------------------------------s.
+static void gmain_split00() 
+{
+	int ponnof = 1;
+	mys_base_stringsplit::nestchar constUserFormat[3] = {{"sec05"},{"AAAAAA,BBBBBB,CCCCCC"},{"1,2,3,4,5,6,7"}};
+	mys_base_stringsplit _csnamesp(3),*csnamesp=&_csnamesp;
+	csnamesp->splitDataRegisterPushued(constUserFormat,-ponnof);
+	if(ponnof==1) {
+		for(int x = 0; x < csnamesp->unitXLine; x++) {
+			printf("%d(x): %2d(s): ", x, csnamesp->unutYLine[x]);
+			for(int y = 0; y < csnamesp->unutYLine[x]; y++) {
+				printf("%s ", csnamesp->split2darry[x][y]);
+			} 	printf("\n");
+		}
+		printf("---------------------------(end)\n");
+	}
+}//---------------------------------------------------------------------e.
+
+//---------------------------------------------------------------------s.
+static void gmain_split01()
+{
+	int ponnof = 1;
+	mys_base_stringsplit::nestchar constUserFormat[4][3] = {
+		{{"sec05,sec15"},{"AAAAAA,BBBBBB,CCCCCC"},{"0,0,0"}},
+		{{"sec15"},{"AAAAAA,BBBBBB,CCCCCC"},{"1,2,3"}},
+		{{"sec05"},{"AAAAAA,BBBBBB,CCCCCC"},{"1,2,3,4,5,6,7"}},
+		{{"sec05"},{"AAAAAA,BBBBBB,CCCCCC"},{"1,2,3,4,5,6,7"}},
+	};
+	for(int ruz = 0; ruz < 4; ruz++) {
+		mys_base_stringsplit _csnamesp(3),*csnamesp=&_csnamesp;
+		csnamesp->splitDataRegisterPushued(constUserFormat[ruz],-ponnof);
+		if(ponnof==1) {
+			for(int x = 0; x < csnamesp->unitXLine; x++) {
+				printf("%d(x): %2d(s): ", x, csnamesp->unutYLine[x]);
+				for(int y = 0; y < csnamesp->unutYLine[x]; y++) {
+					printf("%s ", csnamesp->split2darry[x][y]);
+				} 	printf("\n");
+			}
+			printf("---------------------------(ruz)\n");
+		}
+	}
+}//---------------------------------------------------------------------e.
+
+//---------------------------------------------------------------------s.
+static void gmain_split02() 
+{
+	int ponnof = 1;
+	mys_base_stringsplit::nestchar constUserFormat[4][3] = {
+		{{"sec05,sec15"},{"AAAAAA,BBBBBB,CCCCCC"},{"0,0,0"}},
+		{{"sec15"},{"AAAAAA,BBBBBB,CCCCCC"},{"1,2,3"}},
+		{{"sec25"},{"AAAAAA,BBBBBB,CCCCCC"},{"1,2,3,4,5,6,7"}},
+		{{"sec35"},{"dict,NewralNetwork"},{"10,256,256,512,512,10"}},
+	};
+	mys_delived_stringsplit _csnamesp(4,3),*csnamesp=&_csnamesp;
+	for(int ruz = 0; ruz < csnamesp->requserzLine; ruz++) {
+		csnamesp->splitDataRegisterPushued( &csnamesp->requestuserarray[ruz], constUserFormat[ruz], -ponnof);
+	}
+	if(ponnof==1) {
+		for(int ruz = 0; ruz < csnamesp->requserzLine; ruz++) {
+			for(int x = 0; x < csnamesp->requestuserarray[ruz].unitXLine; x++) {
+				printf("%d(x): %2d(s): ", x, csnamesp->requestuserarray[ruz].unutYLine[x]);
+				for(int y = 0; y < csnamesp->requestuserarray[ruz].unutYLine[x]; y++) {
+					printf("%s ", csnamesp->requestuserarray[ruz].split2darry[x][y]);
+				} 	printf("\n");
+			}
+			printf("---------------------------%d(ruz)\n",ruz);
+		}
+	}
+}//---------------------------------------------------------------------e.
+
+//----------------------------------s.
+static void gmain() {
+	//gmain_split00();
+	//gmain_split01();
+	gmain_split02();
+}//---------------------------------e.
+
+//----------------------------------s.
+int main( int argc , char **argv ) {
+	printf("int main( int argc , char **argv );\n");
+	gmain();
+return 0;
+}//----------------------------------e.
---
+++ chat_array_split_parameter/stringsplit.h
@@ -0,0 +1,113 @@
+﻿//./stringsplit.h
+#ifndef STRINGSPLIT_H
+#define STRINGSPLIT_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+//---------------------------------------------------s.
+template <class myType>
+myType **mymemory(myType**CharMemory2d, int Xsize) {
+	CharMemory2d = new myType*[Xsize];
+	for(int x = 0; x < Xsize; x++) {
+		CharMemory2d[x] = new myType[1024];
+	}
+	return CharMemory2d;
+}//--------------------------------------------------e.
+//---------------------------------------------------s.
+template <class myType>
+void myfree(myType**CharMemory2d,int uaFreeSize) {
+	for(int x = 0; x < uaFreeSize; x++) {
+		delete [] CharMemory2d[x];
+	}delete [] CharMemory2d;
+}//--------------------------------------------------e.
+//---------------------------------------------------s.
+template <class myType>
+myType *mymemory(myType*intMemory1d, int Xsize) {
+	intMemory1d = new myType[Xsize];
+	return intMemory1d;
+}//--------------------------------------------------e.
+//---------------------------------------------------s.
+template <class myType>
+void myfree(myType*intMemory1d) {
+	delete [] intMemory1d;
+}//--------------------------------------------------e.
+//---------------------------------------------------s.
+template <class myType>
+myType***mymemory3d(myType***CharMemory3d, int SizeXLine, int *SizeYvector) {
+	CharMemory3d = new myType**[SizeXLine];
+	for(int x = 0; x < SizeXLine; x++) {
+		CharMemory3d[x] = new myType*[SizeYvector[x]];
+		for(int y = 0; y < SizeYvector[x]; y++) {
+			CharMemory3d[x][y] = new myType[1024];
+			CharMemory3d[x][y][0] = '\0';
+		}
+	}
+	return CharMemory3d;
+}//--------------------------------------------------e.
+//---------------------------------------------------s.
+template <class myType>
+void myfree3d(myType***CharMemory3d, int SizeXLine, int *SizeYvector) {
+	for(int x = 0; x < SizeXLine; x++) {
+		for(int z = 0; z < SizeYvector[x]; z++) {
+			delete [] CharMemory3d[x][z];
+		} delete [] CharMemory3d[x];
+	} delete [] CharMemory3d;
+}//---------------------------------------------------e.
+
+//------------------------------------------------------------s.
+typedef struct mys_base_stringsplit
+{
+	char charArray[1024];
+	typedef struct nestchar{
+		char charArray[1024];
+	}nestchar;
+	int unitXLine = NULL;
+	int *unutYLine = NULL;
+	char **UserAnyArray1d = NULL;
+	char ***split2darry = NULL;
+	virtual void splitDataRegisterPushued(int ponoff);
+	virtual void splitDataRegisterPushued(nestchar *request, int ponoff);
+	void allfree() {
+		myfree3d(split2darry, unitXLine, unutYLine);
+		myfree(UserAnyArray1d, unitXLine);
+		myfree(unutYLine);
+	}
+	mys_base_stringsplit() {}
+	mys_base_stringsplit(int InunitXLine) {
+		unitXLine = InunitXLine;
+	}
+	//~mys_base_stringsplit() {}
+} mys_base_stringsplit;
+//------------------------------------------------------------e.
+
+//------------------------------------------------------------s.
+typedef struct mys_delived_stringsplit : public mys_base_stringsplit
+{
+	int requserzLine=0;
+	mys_base_stringsplit *requestuserarray=NULL;
+	void splitDataRegisterPushued(mys_base_stringsplit *requestuserarray, int ponoff){
+		requestuserarray->splitDataRegisterPushued(ponoff);
+	}
+	void splitDataRegisterPushued(mys_base_stringsplit *requestuserarray, mys_base_stringsplit::nestchar *request, int ponoff){
+		requestuserarray->splitDataRegisterPushued(request,ponoff);
+	}
+	using mys_base_stringsplit::mys_base_stringsplit;
+	mys_delived_stringsplit(int Inruz, int InunitXLine) : mys_base_stringsplit::mys_base_stringsplit(InunitXLine) {
+		requserzLine = Inruz;
+		requestuserarray = new mys_base_stringsplit[Inruz];
+		for(int ruz = 0; ruz < requserzLine; ruz++) {
+			requestuserarray[ruz].unitXLine = InunitXLine;
+		}
+	}
+	~mys_delived_stringsplit() {
+		if(requserzLine!=0) {
+			for(int ruz = 0; ruz < requserzLine; ruz++) {
+				requestuserarray[ruz].allfree();
+			} delete [] requestuserarray;
+		}
+	}
+}mys_delived_stringsplit;
+//------------------------------------------------------------e.
+
+#endif
